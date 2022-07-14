cfc12afbc35c7ace065b18e8aa1da812acd0b097
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-07-13 21:05:32 -0300

    [REQ-20]Cria contador de caracteres

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const main = document.getElementsByTagName('main')[0];
 const formularioLogin = document.getElementsByClassName('trybewarts-login')[0];
 const submiBtn = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
+const textArea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
+const evaluationForm = document.getElementById('evaluation-form');
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
+const house = document.getElementById('house');
+const radioContainer = document.getElementById('radio-container');
+const counterValue = Number(counter.innerText);
 
 formularioLogin.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -23,3 +32,27 @@ agreement.addEventListener('click', (event) => {
     submiBtn.disabled = true;
   }
 });
+
+textArea.addEventListener('input', () => {
+  const tamanhoTexto = counterValue - textArea.value.length;
+  counter.innerText = tamanhoTexto;
+});
+
+submiBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  evaluationForm.style.display = 'none';
+
+  const formData = document.createElement('section');
+  formData.id = 'form-data';
+  main.appendChild(formData);
+
+
+});
+
+function pegarFamilia () {
+  for (let index = 0; index < radioContainer.children.length; index += 1) {
+    if(radioContainer.children[index].firstElementChild.checked) {
+      return radioContainer.children[index].firstElementChild.value;
+    }
+  }
+}
