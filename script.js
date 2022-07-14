3b7216077db47012b761b545a62292e3b90247af
Victor Mendes <victorutdsim7@gmail.com>
2022-07-13 22:32:59 -0300

    [REQ-22] Substitua o formulário pelas informações da pessoa estudante.

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const inputName = document.getElementById('input-name');
 const inputEmail = document.getElementById('input-email');
 const house = document.getElementById('house');
 const radioContainer = document.getElementById('radio-container');
+const contentContainer = document.getElementById('content-container');
+const inputLastname = document.getElementById('input-lastname');
+const radioRateContainer = document.getElementById('radio-rate-container');
+
 const counterValue = Number(counter.innerText);
 
 formularioLogin.addEventListener('submit', (event) => {
@@ -38,21 +42,61 @@ textArea.addEventListener('input', () => {
   counter.innerText = tamanhoTexto;
 });
 
-submiBtn.addEventListener('click', (event) => {
-  event.preventDefault();
-  evaluationForm.style.display = 'none';
-
-  const formData = document.createElement('section');
-  formData.id = 'form-data';
-  main.appendChild(formData);
-
-
-});
-
-function pegarFamilia () {
+function pegarFamilia() {
   for (let index = 0; index < radioContainer.children.length; index += 1) {
-    if(radioContainer.children[index].firstElementChild.checked) {
+    if (radioContainer.children[index].firstElementChild.checked) {
       return radioContainer.children[index].firstElementChild.value;
     }
   }
 }
+
+function pegarConteudo() {
+  const checkedArray = [];
+  const listaConteudo = contentContainer.children;
+  for (let index = 0; index < listaConteudo.length; index += 1) {
+    if (listaConteudo[index].firstElementChild.checked) {
+      checkedArray.push(` ${listaConteudo[index].firstElementChild.value}`);
+    }
+  }
+  return checkedArray;
+}
+
+function criarCampos(chave, valor, formdata) {
+  const paragrafo = document.createElement('p');
+  paragrafo.innerText = `${chave}: ${valor}`;
+  formdata.appendChild(paragrafo);
+}
+
+function pegarNota() {
+  for (let index = 0; index < radioRateContainer.children.length; index += 1) {
+    if (radioRateContainer.children[index].checked) {
+      return radioRateContainer.children[index].value;
+    }
+  }
+}
+
+function tiraFormulario(event) {
+  event.preventDefault();
+  evaluationForm.style.display = 'none';
+}
+
+submiBtn.addEventListener('click', (event) => {
+  tiraFormulario(event);
+  const formData = document.createElement('section');
+  formData.id = 'form-data';
+  main.appendChild(formData);
+  const info = {
+    Nome: `${inputName.value} ${inputLastname.value}`,
+    Email: inputEmail.value,
+    Casa: house.value,
+    Família: pegarFamilia(),
+    Matérias: pegarConteudo(),
+    Avaliação: pegarNota(),
+    Observações: textArea.value,
+  };
+  const chaves = Object.keys(info);
+  const valores = Object.values(info);
+  for (let index = 0; index < chaves.length; index += 1) {
+    criarCampos(chaves[index], valores[index], formData);
+  }
+});
